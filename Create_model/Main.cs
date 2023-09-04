3f9ca7aa1c9adb43f2880483b6c72e3df0523020
andreyzr43 <andreyzr43@yandex.ru>
2023-09-04 07:41:19 +0300

    Добавьте файлы проекта.

---
+++ Create_model/Main.cs
@@ -0,0 +1,36 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Create_model
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Document doc = uidoc.Document;
+
+
+            var res1=new FilteredElementCollector(doc)
+                //.OfClass(typeof(Wall))
+                //.Cast<Wall>()
+                .OfType<Wall>()
+                .ToList();
+            
+
+
+
+
+            return Result.Succeeded;
+        }
+    }
+}
