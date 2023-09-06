014f92767038f8e243c1796f572a485c5d56726a
andreyzr43 <andreyzr43@yandex.ru>
2023-09-06 14:07:06 +0300

    1

--- Create_model/Main.cs
+++ Create_model/Main.cs
@@ -50,7 +50,7 @@ namespace Create_model
             AddWindows(doc, level1, walls[1]);
             AddWindows(doc, level1, walls[2]);
             AddWindows(doc, level1, walls[3]);
-            AddRoof2(doc, level2, walls, uidoc);
+            AddRoof2(doc, level2, walls, uidoc, width);
 
             transaction.Commit();
 
@@ -103,7 +103,7 @@ namespace Create_model
                 footPrintRoof.set_SlopeAngle(m, 0.5);
             }
         }
-        private void AddRoof2(Document doc, Level level2, List<Wall> walls, UIDocument uidoc)
+        private void AddRoof2(Document doc, Level level2, List<Wall> walls, UIDocument uidoc,double width)
         {
             RoofType roofType = new FilteredElementCollector(doc)
                 .OfClass(typeof(RoofType))
@@ -120,35 +120,34 @@ namespace Create_model
             Curve curve1 = locationCurve1.Curve;
             XYZ point1=curve1.GetEndPoint(0);
 
-            LocationCurve locationCurve2 = walls[3].Location as LocationCurve;
+            LocationCurve locationCurve2 = walls[2].Location as LocationCurve;
             Curve curve2 = locationCurve2.Curve;
-            XYZ point2 = curve2.GetEndPoint(0);
-
-
+            XYZ point2 = curve2.GetEndPoint(1);
 
             Application application = doc.Application;
             CurveArray curveArray = new CurveArray();
 
-
-
-            double pointX = ((point1 + point2) / 2).X;
+            double pointX = ((point1+ point2)/2).X;
             double pointY = ((point1 + point2) / 2).Y;
-            double pointZ = (((point1 + point2) / 2).Z)+10;
+            double pointZ = ((point1 + point2) / 2).Z+10;
 
             XYZ pointHigh=new XYZ(pointX, pointY, pointZ);
 
+            Parameter par = roofType.LookupParameter("Толщина");
+            double thickness = par.AsDouble();
 
-            Line line1 = Line.CreateBound(point1, pointHigh);
-            Line line2 = Line.CreateBound(pointHigh, point2);
+            XYZ _point1=CreateNewPoint(point1, dt, level2, -1, thickness);
+            XYZ _point2=CreateNewPoint(point2, dt, level2, + 1, thickness);
+            XYZ _pointHigh=CreateNewPoint(pointHigh, dt, level2);
+
+            Line line1 = Line.CreateBound(_point1, _pointHigh); 
+            Line line2 = Line.CreateBound(_pointHigh, _point2);
+
             curveArray.Append(line1);
             curveArray.Append(line2);
 
-            ReferencePlane plane = doc.Create.NewReferencePlane(point1, point2, pointHigh, doc.ActiveView);
-            SketchPlane sp = SketchPlane.Create(doc, plane.Id);
-            uidoc.ActiveView.SketchPlane = sp;
-            uidoc.ActiveView.ShowActiveWorkPlane();
-
-            //doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, 0, 40);
+            ReferencePlane plane = doc.Create.NewReferencePlane(_point2,_point1, new XYZ(0, 0, 1), doc.ActiveView);
+            doc.Create.NewExtrusionRoof(curveArray, plane, level2, roofType, 0, width+ wallWight);
         }
         private void AddDoor(Document doc, Level level1, Wall wall)
         {
@@ -226,17 +225,23 @@ namespace Create_model
 
             return walls;
         }
-
-        private XYZ CreatePoint(Wall wall, double dt, int n)
+        private XYZ CreateNewPoint(XYZ _point, double dt, Level level = null,int t=1, double thickness=0)
         {
-            LocationCurve locationCurve = wall.Location as LocationCurve;
-            Curve curve = locationCurve.Curve;
-            XYZ _point = curve.GetEndPoint(n);
 
-            double pointX = _point.X;
-            double pointY = _point.Y;
-            double pointZ = _point.Z;
+            double pointX = _point.X - dt;
+            double pointY = _point.Y+(dt*t);
+            double pointZ=0;
 
+            if (level != null)
+            {
+                double with = level.Elevation;
+                pointZ = _point.Z + with+ thickness;
+            }
+            else
+            {
+                pointZ = _point.Z;
+            }
+                 
             XYZ point = new XYZ(pointX, pointY, pointZ);
 
 
